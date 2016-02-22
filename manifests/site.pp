2fe723e8f0057121343184a237612dea2d71b4c8
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-22 14:45:08 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,12 +44,10 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { '/etc/motd':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Hey, Puppet is lots of fun!\n----------------\n\n",
+  exec { 'cowmotd':
+    command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
+    creates => '/etc/motd',
+    path    => '/usr/local/bin',
   }
   
   package { 'cowsay':
