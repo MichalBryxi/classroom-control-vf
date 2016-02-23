108ecf6c7d959c8f7d2c1a07d939ab37e53c5c2d
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-23 14:22:39 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,2 +1,30 @@
 class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure => present,
+    source => 'puppet:///modules/nginx/nginx.conf',
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => present,
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  file { '/var/www/index.html':
+    ensure => present,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
+  service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf'],
+    require   => Package['nginx'],
+  }
 }
