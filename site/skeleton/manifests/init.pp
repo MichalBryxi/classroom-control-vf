ab7bea73d0ecf6c834901336ac0d180a49d16a36
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-23 11:47:19 +0100

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -5,6 +5,6 @@ class skeleton {
   
   file { '/etc/skel/.bashrc':
     ensure => present,
-    source => "puppet:///${module_name}/bashrc",
+    source => "puppet:///modules/skeleton/bashrc",
   }
 }
