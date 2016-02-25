dc2d8d163c6196043bde6fe3ebfdbdc313b2ed6f
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-25 11:58:07 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,13 @@
 class nginx (
-  $service_user     = $::nginx::params::service_user
-  $log_dir          = $::nginx::params::log_dir
-  $package          = $::nginx::params::package
-  $file_owner       = $::nginx::params::file_owner
-  $file_group       = $::nginx::params::file_group
-  $config_dir       = $::nginx::params::config_dir
-  $server_block_dir = $::nginx::params::server_block_dir
-  $service_name     = $::nginx::params::service_name
-  $root             = $::nginx::params::root
+  $service_user     = $::nginx::params::service_user,
+  $log_dir          = $::nginx::params::log_dir,
+  $package          = $::nginx::params::package,
+  $file_owner       = $::nginx::params::file_owner,
+  $file_group       = $::nginx::params::file_group,
+  $config_dir       = $::nginx::params::config_dir,
+  $server_block_dir = $::nginx::params::server_block_dir,
+  $service_name     = $::nginx::params::service_name,
+  $root             = $::nginx::params::root,
 ) inherits nginx::params {
   $document_root = $root
 
