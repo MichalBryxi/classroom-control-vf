ceaf048c6a2fac6f62864fd35700d82a496cde39
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-25 11:51:00 +0100

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,27 +2,6 @@ class nginx (
   $root = '/var/www'
 ) inherits nginx::params {
   $document_root = $root
-  
-  case $operatingsystem {
-    'CentOS', 'RedHat': {
-      $service_user = 'nginx'
-      $log_dir = '/var/log/nginx'
-      $package = 'nginx'
-      $file_owner = 'root'
-      $file_group = 'root'
-      $config_dir = '/etc/nginx'
-      $server_block_dir = '/etc/nginx/conf.d'
-      $service_name = 'nginx'
-    }
-    'Debian': {
-      $document_root = '/var/www'
-      # ...
-    }
-    'Windows': {
-      $document_root = 'C:/ProgramData/nginx/html'
-      # ...
-    }
-  }
 
   File {
     owner => $file_owner,
